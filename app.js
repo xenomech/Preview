1f23759e75f83ba207ddd59090b1383b47f10b1d
xenomech <gokulsmenon227@gmail.com>
2022-08-12 05:54:49 -0700

    ⚡️ added views

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
+app.set("view engine", "ejs")
 app.use(require("./routes/index"))
 app.get('/', (req, res) => {
-  res.send("success")
+  res.render("index")
 })
 app.listen(port, () => {
   console.log(`App on port ${port}`)
