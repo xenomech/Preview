541f02349adaa1b0bc08acb63eac74c7c8856216
Gokul Suresh <50659225+xenomech@users.noreply.github.com>
2022-08-12 08:24:30 -0700

    🔥 Views stage 1 (#1)

---
+++ helpers/constants.js
@@ -0,0 +1,28 @@
+const breakpoints = [
+  {
+    breakpoint: "2xl",
+    height: "",
+    width: ""
+  },
+  {
+    breakpoint: "xl",
+    height: "",
+    width: ""
+  }, {
+    breakpoint: "lg",
+    height: "",
+    width: ""
+  }, {
+    breakpoint: "md",
+    height: "",
+    width: ""
+  }, {
+    breakpoint: "sm",
+    height: "",
+    width: ""
+  }
+]
+
+const themes = {
+
+}
